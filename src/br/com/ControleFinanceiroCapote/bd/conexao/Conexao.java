c51f9c966a9ee76abce2d74801105528a992f7a1
Wander Capote <wanderkpot@gmail.com>
2016-08-30 21:57:32 -0300

    note

--- src/br/com/ControleFinanceiroCapote/bd/conexao/Conexao.java
+++ src/br/com/ControleFinanceiroCapote/bd/conexao/Conexao.java
@@ -10,7 +10,7 @@ public class Conexao {
 		try {
 			Class.forName("org.gjt.mm.mysql.Driver");
 			conexao = java.sql.DriverManager
-					.getConnection("jdbc:mysql://localhost:3306/financeirodb", "root", "root");
+					.getConnection("jdbc:mysql://localhost:3306/financeirodb", "root", "");
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
